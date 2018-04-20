c4c6896b59159b21034868fbd453b8a1c55f71ae
Hafizh Budiman <hafizh.budiman@gmail.com>
2018-04-21 05:30:54 +0700

    Aquarium done error check

--- ArkavQuarium/src/com/ArkavQuarium/Aquarium.java
+++ ArkavQuarium/src/com/ArkavQuarium/Aquarium.java
@@ -2,6 +2,11 @@ package com.ArkavQuarium;
 import java.util.Random;
 
 public class Aquarium {
+    public static final int EGG_PRICE = 500;
+    public static final int GUPPY_PRICE = 25;
+    public static final int PIRANHA_PRICE = 100;
+    public static final double COIN_RADIUS = 25.0;
+
     protected Point maxLocation = new Point();
     protected LinkedList<Guppy> listGuppy = new LinkedList<Guppy>();
     protected LinkedList<Piranha> listPiranha = new LinkedList<Piranha>();
@@ -128,6 +133,118 @@ public class Aquarium {
             return closest;
         }
         return new Point(0,0);
+    }
+
+    public void moveSnail() {
+        if (!listCoin.isEmpty()) {
+            Point locSnail = new Point(garry.getX(), garry.getY());
+            Point closest = getClosestCoin(locSnail);
+            Bool eat = new Bool();
+            garry.move(closest, eat);
+            if (eat.getValue()) {
+                for (int i = 0; i < listCoin.getSize(); i++) {
+                    if (listCoin.get(i).getX() == closest.getX() && listCoin.get(i).getY() == closest.getY()) {
+                        garry.takeCoin(listCoin, listCoin.get(i));
+                    }
+                }
+            }
+        }
+    }
+
+    public void moveGuppy() {
+        Point locGuppy = new Point();
+        Point closest = new Point();
+
+        for (int i = 0; i < listGuppy.getSize(); i++) {
+            if (listGuppy.get(i).isTimeToDie(getCurrentTime())) {
+                listGuppy.remove(listGuppy.get(i));
+            } else {
+                if (listGuppy.get(i).isTimeToGiveCoin(getCurrentTime())) {
+                    Coin coin = new Coin(new Point(listGuppy.get(i).getX(), listGuppy.get(i).getY()), listGuppy.get(i).getGrowthStep());
+                    listCoin.add(coin);
+                }
+
+                if (listFood.getSize() > 0 && listGuppy.get(i).isHungry(getCurrentTime())) {
+                    locGuppy.setX(listGuppy.get(i).getX());
+                    locGuppy.setY(listGuppy.get(i).getY());
+                    Point temp = getClosestFood(locGuppy);
+                    closest.setX(temp.getX());
+                    closest.setY(temp.getY());
+
+                    for (int idx = 0; idx < listFood.getSize(); idx++) {
+                        if (listFood.get(idx).getX() == closest.getX() && listFood.get(idx).getY() == closest.getY() ) {
+                            listGuppy.get(i).moveToEat(listFood, listFood.get(idx), getCurrentTime());
+                            break;
+                        }
+                    }
+                } else {
+                    listGuppy.get(i).moveRandom(getCurrentTime(), maxLocation);
+                }
+            }
+        }
+    }
+
+    public void movePiranha() {
+        Point locPiranha = new Point();
+        Point closest = new Point();
+
+        for (int i = 0; i < listPiranha.getSize(); i++) {
+            if (listPiranha.get(i).isTimeToDie(getCurrentTime())) {
+                listPiranha.remove(listPiranha.get(i));
+            } else {
+                if (!listGuppy.isEmpty() && listPiranha.get(i).isHungry(getCurrentTime())) {
+                    Bool eat = new Bool();
+                    int guppyGrowth = 0;
+
+                    locPiranha.setX(listPiranha.get(i).getX());
+                    locPiranha.setY(listPiranha.get(i).getY());
+
+                    Point temp = getClosestGuppy(locPiranha);
+
+                    closest.setX(temp.getX());
+                    closest.setY(temp.getY());
+
+                    for (int idx = 0; idx < listGuppy.getSize(); idx++) {
+                        if (listGuppy.get(idx).getX() == closest.getX() && listGuppy.get(idx).getY() == closest.getY()) {
+                            guppyGrowth = listGuppy.get(idx).getGrowthStep();
+                            listPiranha.get(i).moveToEat(listGuppy, listGuppy.get(idx), getCurrentTime());
+
+                            if (eat.getValue()) {
+                                guppyGrowth++;
+                                Coin coin = new Coin(closest, guppyGrowth);
+                                listCoin.add(coin);
+                            }
+                            break;
+                        }
+                    }
+
+                } else {
+                    listPiranha.get(i).moveRandom(getCurrentTime(), maxLocation);
+                }
+            }
+        }
+    }
+
+    public void moveCoin() {
+        for (int i = 0; i < listCoin.getSize(); i++) {
+            listCoin.get(i).move(maxLocation.getY());
+        }
+    }
+
+    public void moveFood() {
+        for (int i = 0; i < listFood.getSize(); i++) {
+            listFood.get(i).move(maxLocation.getY());
+
+            if (listFood.get(i).isAtBottom(maxLocation.getY())) {
+                listFood.remove(listFood.get(i));
+            }
+        }
+    }
+
+    public int getStateGame() {
+        if (egg == 3) return 2;
+        else if ((money < GUPPY_PRICE) && listGuppy.isEmpty() && listCoin.isEmpty()) return 1;
+        else return 0;
     }
 
 }
