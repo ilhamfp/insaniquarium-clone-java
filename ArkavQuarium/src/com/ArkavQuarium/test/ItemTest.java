b2c2ea6e79e9b128189e361c2d0ae9dead29a608
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-04-20 23:35:37 +0700

    tambah test

--- ArkavQuarium/src/com/ArkavQuarium/test/ItemTest.java
+++ ArkavQuarium/src/com/ArkavQuarium/test/ItemTest.java
@@ -1,11 +1,29 @@
 package com.ArkavQuarium.test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.ArkavQuarium.Coin;
+import com.ArkavQuarium.Point;
 import org.junit.jupiter.api.Test;
 
 class ItemTest {
     @Test
-    public void test() {
-        assertEquals(6,5+1);
+    public void testConstructorCoinEmpty() {
+        Coin c = new Coin();
+        double expectedX = -1.0;
+        double expectedY = -1.0;
+        assertEquals(expectedX,c.getX());
+        assertEquals(expectedY,c.getY());
+
+    }
+
+    @Test
+    public void testConstructorCoinWithParameters() {
+        Point p = new Point(2.0,3.0);
+        Coin c = new Coin(p,3);
+        double expectedX = 2.0;
+        double expectedY = 3.0;
+        assertEquals(expectedX,c.getX());
+        assertEquals(expectedY,c.getY());
     }
 }
