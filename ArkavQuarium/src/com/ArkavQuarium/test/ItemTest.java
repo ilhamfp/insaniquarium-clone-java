1cd34a37caf4f7bc1227a6d83c0ae9eb3ce0fda6
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-04-20 23:38:15 +0700

    Fix

--- ArkavQuarium/src/com/ArkavQuarium/test/ItemTest.java
+++ ArkavQuarium/src/com/ArkavQuarium/test/ItemTest.java
@@ -2,28 +2,8 @@ package com.ArkavQuarium.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.ArkavQuarium.Coin;
-import com.ArkavQuarium.Point;
 import org.junit.jupiter.api.Test;
 
 class ItemTest {
-    @Test
-    public void testConstructorCoinEmpty() {
-        Coin c = new Coin();
-        double expectedX = -1.0;
-        double expectedY = -1.0;
-        assertEquals(expectedX,c.getX());
-        assertEquals(expectedY,c.getY());
 
-    }
-
-    @Test
-    public void testConstructorCoinWithParameters() {
-        Point p = new Point(2.0,3.0);
-        Coin c = new Coin(p,3);
-        double expectedX = 2.0;
-        double expectedY = 3.0;
-        assertEquals(expectedX,c.getX());
-        assertEquals(expectedY,c.getY());
-    }
 }
