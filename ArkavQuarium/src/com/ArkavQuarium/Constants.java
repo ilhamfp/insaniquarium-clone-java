140cb9461a33b8c063b2755465cd0c645d4ea7b4
Shevalda Gracielira <nightlyfurious@gmail.com>
2018-04-21 02:04:08 +0700

    guppytest

--- ArkavQuarium/src/com/ArkavQuarium/Constants.java
+++ ArkavQuarium/src/com/ArkavQuarium/Constants.java
@@ -4,4 +4,8 @@ public final class Constants {
     public static final int COIN_BASE_VALUE = 15;
     public static final int FOOD_PRICE = 5;
     public static final double ITEM_SPEED = 0.6;
+    public static final double GUPPY_SPEED = 1.25;
+    public static final double GUPPY_HUNGRY_CONSTRAINT = 10.0;
+    public static final double GUPPY_FULL_CONSTRAINT = 6.0;
+    public static final double  PIRANHA_PRICE = 100;
 }
