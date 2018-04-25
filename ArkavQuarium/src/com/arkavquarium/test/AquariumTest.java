67ddff8c416b67df1c856747e55690f38a6c410b
Hafizh Budiman <hafizh.budiman@gmail.com>
2018-04-25 22:07:26 +0700

    FIX NAME

--- ArkavQuarium/src/com/arkavquarium/test/AquariumTest.java
+++ ArkavQuarium/src/com/arkavquarium/test/AquariumTest.java
@@ -27,12 +27,34 @@ class AquariumTest {
     assertEquals(0,aquarium.getStateGame());
   }
 
+  @Test
   public void testGetClosestCoin() {
     Aquarium aquarium = new Aquarium(680,720);
     Point coin = new Point(10.0,10.0);
     aquarium.createCoin(coin, 1);
     Point closest = aquarium.getClosestCoin(new Point(11.0, 11.0));
-    assertEquals(closest.getX(), );
+    assertEquals(closest.getX(), coin.getX());
+    assertEquals(closest.getY(), coin.getY());
   }
+
+  @Test
+  public void testGetCLosestGuppy() {
+    Aquarium aquarium = new Aquarium(680,720);
+    aquarium.createGuppy();
+    Point closest = aquarium.getClosestGuppy(new Point(11.0, 11.0));
+    assertEquals(closest.getX(), aquarium.getListGuppy().get(0).getX());
+    assertEquals(closest.getY(), aquarium.getListGuppy().get(0).getY());
+  }
+
+  @Test
+  public void testGetClosestFood() {
+    Aquarium aquarium = new Aquarium(680,720);
+    Point food = new Point(10.0,10.0);
+    aquarium.createFood(food);
+    Point closest = aquarium.getClosestFood(new Point(11.0, 11.0));
+    assertEquals(closest.getX(), food.getX());
+    assertEquals(closest.getY(), food.getY());
+  }
+
 
 }
